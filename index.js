808983309f0fda3e22b8037e3e7b9bfc6044f363
Dave McKenna <davemckenna01@gmail.com>
2016-02-27 00:12:24 -0500

    add a LOT

--- index.js
+++ index.js
@@ -8,211 +8,165 @@ var app = express();
 
 // this would be in a data store somewhere on the server
 var datastore1 = {
-    "docs": {
-        1: {"id": 1, "user": 1, "filename": "bell_bill.pdf", "size": 73621},
-        2: {"id": 2, "user": 1, "filename": "aws_bill.pdf", "size": 63291},
-        3: {"id": 3, "user": 1, "filename": "github_bill.pdf", "size": 13521},
-        4: {"id": 4, "user": 2, "filename": "rogers_bill.pdf", "size": 91921},
-        5: {"id": 5, "user": 2, "filename": "azure_bill.pdf", "size": 83761}
-    },
-    "tags": {
-        1: {
-            "id": 1,
-            "user": 1,
-            "name": "work expenses",
-            "documents": [1, 2, 3]
-        },
-        2: {
-            "id": 2,
-            "user": 1,
-            "name": "internet",
-            "documents": [2, 3]
-        },
-        3: {
-            "id": 3,
-            "user": 2,
-            "name": "microsoft",
-            "documents": [5]
-        }
-    }
+    "docs": [
+        {"id": 1, "user": 1, "filename": "bell_bill.pdf", "size": 73621},
+        {"id": 2, "user": 1, "filename": "aws_bill.pdf", "size": 63291},
+        {"id": 3, "user": 1, "filename": "github_bill.pdf", "size": 13521},
+        {"id": 4, "user": 2, "filename": "rogers_bill.pdf", "size": 91921},
+        {"id": 5, "user": 2, "filename": "azure_bill.pdf", "size": 83761}
+    ],
+    "tags": [
+        {"id": 1, "user": 1, "name": "work expenses", "documents": [1, 2, 3] },
+        {"id": 2, "user": 1, "name": "internet", "documents": [2, 3] },
+        {"id": 3, "user": 2, "name": "microsoft", "documents": [5] }
+    ]
 }
 
 var datastore2 = {
-    "docs": {
-        1: {"id": 1, "user": 1, "views": 41},
-        2: {"id": 2, "user": 1, "views": 26},
-        3: {"id": 3, "user": 1, "views": 51},
-        4: {"id": 4, "user": 2, "views": 42},
-        5: {"id": 5, "user": 2, "views": 93}
+    "docs": [
+        {"id": 1, "user": 1, "views": 41},
+        {"id": 2, "user": 1, "views": 26},
+        {"id": 3, "user": 1, "views": 51},
+        {"id": 4, "user": 2, "views": 42},
+        {"id": 5, "user": 2, "views": 93}
+    ]
+}
+
+function getItems (datastore, itemType) {
+    return function (pathSet) {
+        var items,
+            itemIds,
+            itemKeys,
+            itemPropName,
+            results;
+
+        if (this.userId == null) {
+            throw new Error("not authorized");
+        }  
+
+        console.log(pathSet);
+
+        // pick out important parts of the request URL
+        itemPropName = pathSet[0];
+        itemIds = _.map(pathSet[1], function(id) {return parseInt(id);});
+        itemKeys = Array.isArray(pathSet[2]) ? pathSet[2] : [pathSet[2]];
+
+        // 1. get requested items constrained by user id (this is our "DB" call)
+        items = _.chain(datastore[itemType])
+                .where({user: this.userId})
+                .filter(function(item) {return _.contains(itemIds, item.id) })
+                .value();
+
+        results = [];
+
+        // 2. for each item id in the request, attempt to create a path
+        itemIds.forEach(function(itemId) {
+            var item;
+
+            item = _.where(items, {id: itemId})[0];
+
+            if (!item) {
+                results.push({
+                    path: [itemPropName, itemId],
+                    value: undefined
+                });
+            } else {
+                itemKeys.forEach(function(itemKey) {
+                    results.push({
+                        // path is something like:
+                        // documentsById[42].filename,
+                        path: [itemPropName, itemId, itemKey],
+                        value: item[itemKey]
+                    });
+                });
+            }
+        })
+
+        return results;
+    }
+}
+
+function getItemsChildren (datastore, itemType) {
+    return function (pathSet) {
+        var items,
+            itemIds,
+            itemPropName,
+            childrenPropName,
+            childIds,
+            results;
+
+        if (this.userId == null) {
+            throw new Error("not authorized");
+        }  
+
+        console.log(pathSet);
+
+        // pick out important parts of the request URL
+        itemPropName = pathSet[0];
+        itemIds = _.map(pathSet[1], function(id) {return parseInt(id);});
+        childrenPropName = pathSet[2];
+        childIds = _.map(pathSet[3], function(id) {return parseInt(id);});
+
+        // 1. get requested items constrained by user id (this is our "DB" call)
+        items = _.chain(datastore[itemType])
+                .where({user: this.userId})
+                .filter(function(item) {return _.contains(itemIds, item.id) })
+                .value();
+
+        results = [];
+
+        // 2. for each item id in the request, attempt to create a path
+        itemIds.forEach(function(itemId) {
+            var item;
+
+            item = _.where(items, {id: itemId})[0];
+
+            if (!item) {
+                results.push({
+                    path: [itemPropName, itemId],
+                    value: undefined
+                });
+            } else {
+                childIds.forEach(function(childId) {
+                    if ( _.contains(item[childrenPropName], childId) ) {
+                        results.push({
+                            // path is something like:
+                            // tagList[42].documents[99],
+                            path: [itemPropName, itemId, childrenPropName, childId],
+                            value: {$type: 'ref', value: [childrenPropName + 'ById', childId]}
+                        });
+                    } else {
+                        results.push({
+                            // path is something like:
+                            // tagList[42].documents[99],
+                            path: [itemPropName, itemId, childrenPropName, childId],
+                            value: undefined
+                        });
+                    }
+                });
+            }
+        })
+
+        return results;
     }
 }
 
 var routes = [
     {
-        route: 'documentsById[{integers:docIds}]["filename", "size"]',
-        // Route handlers are run with the Router instance as their this object
-        get: function(pathSet) {
-            var docs,
-                docIds,
-                docKeys,
-                results;
-
-            if (this.userId == null) {
-                throw new Error("not authorized");
-            }  
-
-            docs = [];
-            docIds = pathSet[1];
-            docKeys = pathSet[2];
-
-            // this is our "DB" call
-            docIds.forEach(function(id) {
-                docs.push(datastore1.docs[id]);
-            });
-
-            // not sure what to do
-            if (docs.length === 0) throw new Error ('oh god no');
-
-            results = [];
-
-            docIds.forEach(function(docId) {
-                docKeys.forEach(function(docKey) {
-                    console.log(docId, docKey);
-
-                    results.push({
-                        path: ['documentsById', docId, docKey],
-                        value: _.where(docs, {id: docId})[0][docKey]
-                    })
-                });
-            })
-
-            return results;
-        }
+        route: 'documentsById[{integers}]["filename", "size"]',
+        get: getItems(datastore1, 'docs')
     },
     {
-        route: 'documentsById[{integers:docIds}].views',
-        // Route handlers are run with the Router instance as their this object
-        get: function(pathSet) {
-            var docs,
-                docIds,
-                docKeys,
-                results;
-
-            if (this.userId == null) {
-                throw new Error("not authorized");
-            }  
-
-            docs = [];
-            docIds = pathSet[1];
-            docKeys = pathSet[2];
-
-            // this is our "DB" call
-            docIds.forEach(function(id) {
-                docs.push(datastore2.docs[id]);
-            });
-
-            // not sure what to do
-            if (docs.length === 0) throw new Error ('oh god no');
-
-            results = [];
-            docKeys = Array.isArray(docKeys) ? docKeys : [docKeys];
-
-            docIds.forEach(function(docId) {
-                docKeys.forEach(function(docKey) {
-                    results.push({
-                        path: ['documentsById', docId, docKey],
-                        value: _.where(docs, {id: docId})[0][docKey]
-                    })
-                });
-            })
-
-            return results;
-        }
+        route: 'documentsById[{integers}].views',
+        get: getItems(datastore2, 'docs')
     },
     {
-        route: 'tagList[{integers:tagIds}].name',
-        // Route handlers are run with the Router instance as their this object
-        get: function(pathSet) {
-            var tags,
-                tagIds,
-                tagKeys,
-                results;
-
-            if (this.userId == null) {
-                throw new Error("not authorized");
-            }  
-
-            tags = [];
-            tagIds = pathSet[1];
-            tagKeys = pathSet[2];
-
-            // this is our "DB" call
-            tagIds.forEach(function(id) {
-                tags.push(datastore1.tags[id]);
-            });
-
-            // not sure what to do
-            if (tags.length === 0) throw new Error ('oh god no');
-
-            results = [];
-            tagKeys = Array.isArray(tagKeys) ? tagKeys : [tagKeys];
-
-            tagIds.forEach(function(tagId) {
-                tagKeys.forEach(function(tagKey) {
-                    results.push({
-                        path: ['tagList', tagId, tagKey],
-                        value: _.where(tags, {id: tagId})[0][tagKey]
-                    })
-                });
-            })
-
-            return results;
-        },
+        route: 'tagList[{integers}].name',
+        get: getItems(datastore1, 'tags')
     },
     {
-        route: 'tagList[{integers:tagIds}].documents[{integers:docIds}]',
-        // Route handlers are run with the Router instance as their this object
-        get: function(pathSet) {
-            var tags,
-                tagIds,
-                docIds,
-                results;
-
-            // if (this.userId == null) {
-            //     throw new Error("not authorized");
-            // }  
-
-            console.log(pathSet);
-
-            tags = [];
-            tagIds = pathSet[1];
-            docIds = pathSet[3];
-
-            // this is our "DB" call
-            tagIds.forEach(function(id) {
-                tags.push(datastore1.tags[id]);
-            });
-
-            console.log(tags);
-
-            // not sure what to do
-            if (tags.length === 0) throw new Error ('oh god no');
-
-            results = [];
-            docIds = Array.isArray(docIds) ? docIds : [docIds];
-
-            tagIds.forEach(function(tagId) {
-                docIds.forEach(function(docId) {
-                    results.push({
-                        path: ['tagList', tagId, 'documents', docId],
-                        value: {$type: 'ref', value: ['documentsById', docId]}
-                    })
-                });
-            })
-
-console.log(results);
-            return results;
-        }
+        route: 'tagList[{integers}].documents[{integers}]',
+        get: getItemsChildren(datastore1, 'tags')
     }    
 ]
 // Create a Router base class
@@ -241,8 +195,8 @@ var server = app.listen(3000);
 
 
 
-var router = new Router(routes);
+// var router = new Router(routes);
 
-router.get([["tagList", [1, 2], ["documents"], [1, 2, 3]]]).subscribe(function(jsonGraph) {
-    console.log(JSON.stringify(jsonGraph, null, 4));
-});
+// router.get([["tagList", [1, 2], ["documents"], [1, 2, 3]]]).subscribe(function(jsonGraph) {
+//     console.log(JSON.stringify(jsonGraph, null, 4));
+// });
